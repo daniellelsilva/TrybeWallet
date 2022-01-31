2be6a9c3a545bf551ff0cc9265e8276ce2621c37
Danielle Silva <danielle.luisasilva@gmail.com>
2022-01-30 23:46:30 -0300

    Updates

---
+++ precache-manifest.237af8b1e9574268fd1814777bb1aee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d9a1ae1d005269f66b01ac5e33b323",
+    "url": "/TrybeWallet/index.html"
+  },
+  {
+    "revision": "e91244eb6b4ca1bb3979",
+    "url": "/TrybeWallet/static/css/main.15028f1c.chunk.css"
+  },
+  {
+    "revision": "3fd85c14b5c839407406",
+    "url": "/TrybeWallet/static/js/2.64f0abf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrybeWallet/static/js/2.64f0abf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91244eb6b4ca1bb3979",
+    "url": "/TrybeWallet/static/js/main.484cc81e.chunk.js"
+  },
+  {
+    "revision": "7064498208332b0843ee",
+    "url": "/TrybeWallet/static/js/runtime-main.e31e136f.js"
+  },
+  {
+    "revision": "6565bfeb355d8a0bc474eb20ba38f1b2",
+    "url": "/TrybeWallet/static/media/undraw_online_payments_re_y8f2.6565bfeb.svg"
+  }
+]);
